ef364ea9f4fa9561ff98e66e7adf9006bdc74370
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-16 15:10:36 -0800

    finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+ * main - this is a function that print a line
+ * Return: 0(Success)
+ * thats it
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 
-	return 0;
+	return (0);
 }
 
