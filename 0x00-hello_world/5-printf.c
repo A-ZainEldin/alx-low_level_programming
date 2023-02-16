b8c75bfa47e25ff83b0f8134cbafd6d43f80eec3
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-16 15:18:11 -0800

    0k

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this is a function that print a line
+ * Return: 0(Success)
+ * thats it
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
+
