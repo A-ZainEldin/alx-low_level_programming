0be4da924b0daf536aa09fd0319273ed345b51c9
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-17 13:20:39 -0800

    ok

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - this is main discription
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+char y;
+y = a;
+x = 0;
+
+while (x < 10)
+{
+	printf("%d", x);
+	x++;
+}
+
+while (y <= 'f')
+{
+	printf("%c", x);
+	y++;
+}
+putchar('\n');
+return (0);
+}
