19a3ab8a235b9429dd7ce3148e597390afaa6a9f
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-17 12:59:23 -0800

    ok

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - this is main discription
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+x=0;
+while (x<10)
+{
+	printf("%d", x);
+	x++
+}
+putchar('\n');
+return (0);
+}
