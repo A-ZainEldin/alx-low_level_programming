99446351280d2948e0cf2267b6454377c3d3684e
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-17 12:41:48 -0800

    ok

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - this is main discription
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char test = 'a';
+while (test <= 'z')
+{
+	if(test != 'q' && test != 'e')
+	putchar(test);
+	test++;
+}
+putchar('\n');
+return (0);
+}
