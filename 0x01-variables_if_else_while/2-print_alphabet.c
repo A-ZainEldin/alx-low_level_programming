c3364902f0c50b1f893b51c718e13fd000fc2d22
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-17 12:29:25 -0800

    ok

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - this is main discription
+ *
+ *Return: Always 0 (Success) 
+ */
+int main(void)
+{
+char test = 'a';
+while (test<='z')
+{
+	putchar(test);
+	test++;
+}
+putchar('\n');
+return (0);
+}				}
