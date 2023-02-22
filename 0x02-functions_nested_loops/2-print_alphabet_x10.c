aa1bb4d7463284c0b25ae034fa4764ef009739be
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-21 17:20:55 -0800

    o

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include"main.h"
 /**
- * prints code 10 times
+ * print_alphabet_x10 - prints code 10 times
  * prints a line
  */
 void print_alphabet_x10(void)
-{	
+{
 	char z = 'a';
 	int x = 0;
 
@@ -16,6 +16,7 @@ void print_alphabet_x10(void)
 			z++;
 		}
 		x++;
+		_putchar('\n');
 		z = 'a';
 	}
 }
