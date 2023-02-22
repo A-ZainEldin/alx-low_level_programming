4b163ea3884e9420a496d6fe36eb00fab7a2e316
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-21 18:14:59 -0800

    ok

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,14 @@
  */
 int print_last_digit(int Z)
 {
-	int x = Z % 10;
+	int a;
+	if (Z < 0)
+		n = -n;
+	a = n % 10;
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0')
+		return (a);
 
-	_putchat(x);
-	return (Z % 10);
 
 }
