e4cb0203a4bbb53153a7e68876d0634b10899618
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-21 16:39:38 -0800

    0o

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * prints code 10 times
+ * prints a line
+ */
+void print_alphabet_x10(void)
+{	
+	char z = 'a';
+	int x = 0;
+	while (x<10)
+	{
+		while (z<123)
+		{
+			_putchar(z);
+			z++;
+		}
+		x++;
+	}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * this is a program that checks if a number is samll
+ */
+int _islower(int c)
+{
+	if (c > 123 || c < 97) 
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
