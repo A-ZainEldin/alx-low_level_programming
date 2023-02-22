5935f2918ea451b43825b7b02cc3e7baea742f25
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-21 17:26:58 -0800

    ok

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,17 @@
 #include"main.h"
 /**
- * this is a program that checks if a number is samll
+ * _islower - this is a program that checks if a number is samll
+ * @c : is the number tested
+ * Return: Always 0 or 1 (Success)
  */
 int _islower(int c)
 {
-	if (c > 123 || c < 97) 
+	if (c > 123 || c < 97)
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return 1;
+		return (1);
 	}
 }
