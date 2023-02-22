cbda3b5b6e311d42c8c5bf242ed2e84045c13de5
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-21 17:32:02 -0800

    ok

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,23 @@
 #include"main.h"
+
 /**
- * checks for letters
- * Return 1 or 0.
+ * _isalpha - checks for letters
+ * Return: 1 or 0.
+ * @c: a number to get checked
  */
 
 int _isalpha(int c)
 {
-	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	if (c > 'a' && c < 'z')
 	{
-		return 1;
+		return (1);
+	}
+	else if (c > 'A' && c < 'Z')
+	{
+		return (1);
 	}
 	else
 	{
-		return 0:
+		return (0);
 	}
 }
