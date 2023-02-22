9e1e12bf7f68c5ae896c93ffb3a0024ca96bb872
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-21 18:42:47 -0800

    0ok

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,20 @@
  */
 void jack_bauer(void)
 {
-	int x,y;
+	int x, y;
+
 	x = 0;
+
 	while (x < 24)
 	{
 		y = 0;
-		while( y < 60)
+		while ( y < 60)
 		{
-			_putchar((x/10) + '0');
-			_putchar((x%10) + '0');
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
 			_putchar(':');
-			_putchar((x/10)+'0');
-			_putchar((x%10)+'0');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 			_putchar('\n');
 			y++;
 		}
