d3f8b2957f0765bfa477fb461f003f8f0e16f772
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-23 07:35:05 -0800

    0k

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - does multiplying
+ * Return: number
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i, i < 10, i++)
+	{
+		_putchar(i)
+	}
+}
