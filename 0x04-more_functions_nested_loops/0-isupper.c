fb9e3385ff61cfa1d069a39301e41ed3c5379863
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-23 05:26:43 -0800

    all

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ *_isupper - checks if the number givven is ubber
+ *@c:the number given
+ *Return:is always one or zero
+ */
+int _isupper(int c)
+{
+	if (c > 90 || c < 65)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
