5dc3f22e205232628c0084268e3c725884324618
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-23 16:06:38 -0800

    ok

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_line - prints a line
+ * @n : integer used
+ * Return : Always a line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i=0 ;i < n ;i++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
