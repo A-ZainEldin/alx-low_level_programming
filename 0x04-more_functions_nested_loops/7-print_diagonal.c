877fab799aba753c875f4f7fd76b5bb48b3c876e
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-23 16:21:33 -0800

    ok

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_diagonal - prints a line
+ * @n : integer used
+ * Return : Always a line
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j <= (i - 1) ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
