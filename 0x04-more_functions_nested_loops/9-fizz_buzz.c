8225ab195c27cfafb33792f6cbf8ee92a907e8ca
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-23 17:02:46 -0800

    ok

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include "main.h"
+#include <stdio.h>
 /**
  *main - fizz buzz test
  *Return: As it is a main funtion return 0 means success
@@ -6,6 +7,7 @@
 int main(void)
 {
 	short int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
