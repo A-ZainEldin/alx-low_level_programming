841b24519049f5e699f60668ee1507275f3df15b
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-23 17:59:29 -0800

    ok

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,44 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-#ifndef main_h
-#define main_h
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days (int month, int day, int year )
-{
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
-
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
-}
-#endif
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif /* MAIN_H */
