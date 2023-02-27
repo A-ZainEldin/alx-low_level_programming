d861fca133a5cdedbdd8a69676ad28a510dd9b90
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-27 08:24:34 -0800

    all

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *rev_string - gives length of passed string
+ *@s: the string passed
+ *Return: Always none
+ */
+void rev_string(char *s)
+{
+	int i;
+	int sum;
+	int j;
+
+	i = 0;
+	sum = 0;
+	j = 0;
+
+	while (s[i] != '\0')
+	{
+		sum = sum + 1;
+		i++;
+	}
+	char var[sum];
+
+	i = sum;
+	while (i != 0)
+	{
+		var[j] = s[i - 1];
+		i--;
+		j++;
+	}
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		s[i] = var[i];
+		i++;
+	}
+	_putchar('\n');
+}
