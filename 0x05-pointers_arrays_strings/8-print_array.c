27c88a79436e4af0b6f1a30c952d56a4a471c3e3
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-27 10:35:41 -0800

    AQ

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,21 +8,24 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
-
-	i = 0;
-
-	while (i != n)
+	if (n > 0)
 	{
-		if (i + 1 == n)
+		int i;
+
+		i = 0;
+
+		while (i != n)
 		{
-			printf("%d", a[i]);
+			if (i + 1 == n)
+			{
+				printf("%d", a[i]);
+			}
+			else
+			{
+				printf("%d, ", a[i]);
+			}
+			i++;
 		}
-		else
-		{
-			printf("%d, ", a[i]);
-		}
-		i++;
 	}
 	printf("\n");
 }
