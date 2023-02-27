5ffba925c6d510149022f28e2d0b4fb7738e39e6
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-27 07:28:37 -0800

    ALL

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - gives length of passed string
+ *@str: the string passed
+ *Return: Always a void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+}
