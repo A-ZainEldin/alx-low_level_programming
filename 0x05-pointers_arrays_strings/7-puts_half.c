a53d903cf3329e4df5ab358fd2949a9137aa39a0
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-27 09:17:28 -0800

    ok

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - gives length of passed string
+ *@str: the string passed
+ *Return: Always an integer
+ */
+void puts_half(char *str)
+{
+	int i;
+	int sum;
+	int stop;
+
+	i = 0;
+	sum = 0;
+
+	while (str[i] != '\0')
+	{
+		sum = sum + 1;
+		i++;
+	}
+	stop = (sum / 2) - 1;
+	i = sum;
+	while (stop != i - 1)
+	{
+		_putchar(str[stop + 1]);
+		stop++;
+	}
+	_putchar('\n');
+}
