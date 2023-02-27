a9fed29d4fec9a88ac3409a5cdd6bb028932dfdd
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-02-27 10:31:27 -0800

    ok

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - gives length of passed string
+ *@a: the string passed
+ *@n: where it stops
+ *Return: Always a void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i != n)
+	{
+		if (i + 1 == n)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
