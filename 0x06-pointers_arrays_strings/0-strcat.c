c847dd4a4ed8c14650170e06e5ecfc1c881eff39
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-01 12:35:10 -0800

    ok

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _strcat- a function that joins 2 strings
+ * @dest: what is appended to
+ * @src: what is being appended
+ * Return: a ponter
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, test;
+
+	i = 0;
+	j = 0;
+	test = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	return (dest);
+}
