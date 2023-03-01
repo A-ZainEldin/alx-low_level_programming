6acea937ee7cf4c55926bd55942123304451e043
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-01 13:13:10 -0800

    0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * _strncat- does casting
+ * @dest: the main
+ * @src: the cocatenated
+ * @n: the amount
+ * Return: always a pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
