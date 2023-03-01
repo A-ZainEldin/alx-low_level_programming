d5d3b6971f91079842cb2af61af8284b7fa19b58
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-01 12:59:22 -0800

    all

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,11 +7,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j, test;
+	int i, j;
 
 	i = 0;
 	j = 0;
-	test = 0;
 
 	while (dest[i] != '\0')
 	{
@@ -21,6 +20,7 @@ char *_strcat(char *dest, char *src)
 	{
 		dest[i] = src[j];
 		j++;
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
