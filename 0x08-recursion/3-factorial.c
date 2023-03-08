ef2e3098be5edcb43844ba28b4eceb76b0078a0f
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-08 09:08:00 -0800

    ok

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * factorial -the number in its lowers
- * @n :the number passed
- * Return :Always a number
+ *factorial-the number in its lowers
+ *@n:the number passed
+ *Return:Always a number
  */
 int factorial(int n)
 {
 	if (n > 0)
 	{
-		return(n * factorial(n-1));
+		return (n * factorial(n - 1));
 	}
 	else if (n == 0)
 	{
