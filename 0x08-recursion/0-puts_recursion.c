291bebfc977abe06d9816a40982ba5ec7e4e87a1
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-08 07:49:59 -0800

    the first try

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function that prints a string
+ * @s : the addres of the first character of the string
+ * Return : always 0
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	_puts_recursion(&(s[i+1]));
+
+}
