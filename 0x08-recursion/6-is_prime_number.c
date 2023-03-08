8621b6fa8aa67d4cdef0021525507bc5781406a1
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-08 13:38:51 -0800

    ok

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,53 @@
 #include "main.h"
 /**
- * test- tests function
- * @factor:factors
- * @tests:manx
- * Return:number
+ * find_max_factor - recursively finds the maximum factor
+ * @n: the number to be checked
+ * @factor: the factor to be tested
+ *
+ * Return: the maximum factor
  */
-int test(int factor, int tests)
+int find_max_factor(int n, int factor)
 {
+	if (factor * factor > n)
+	{
+		return (factor - 2);
+	}
+	return (find_max_factor(n, factor + 2));
+}
+
+/**
+ * test - recursively checks if a number is prime
+ * @factor: the factor to be tested
+ * @tests: the number to be checked
+ * @max_factor: the maximum factor to be tested
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int test(int factor, int tests, int max_factor)
+{
+	if (factor > max_factor)
+	{
+		return (1);
+	}
 	if (tests % factor == 0)
 	{
-		if (factor == tests)
-			return (1);
-		return (test(factor + 1, tests));
+		return (0);
 	}
-	return (0);
+	return (test(factor + 2, tests, max_factor));
 }
+
 /**
- * is_prime_number- does get square root
- * @n: the number needed
- * Return: the value desired from root or -1
+ * is_prime_number - checks if a number is prime
+ * @n: the number to be checked
+ *
+ * Return: 1 if prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-	return (test(1, n));
+	if (n < 2)
+	{
+		return (0);
+	}
+	int max_factor = find_max_factor(n, 1);
+	return (test(3, n, max_factor));
 }
