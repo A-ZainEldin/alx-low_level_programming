225cb328c47d0833873eb5af9ad90fbf1158cd5a
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-08 08:59:15 -0800

    p

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _strlen_recursion -how long is string??
- * @s :string start
- * Return :an int
+ *_strlen_recursion-how long is string??
+ *@s:string start
+ *Return:an int
  */
 int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		return(1+_strlen_recursion(&(s[1])));
+		return (1 + _strlen_recursion(&(s[1])));
 	}
 	else
 	return (0);
