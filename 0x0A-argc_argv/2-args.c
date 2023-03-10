2be936255a1bf6845d23e4e8bba242891ff815e3
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-10 14:56:27 -0800

    p

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ *main- a function that prints its arguments
+ *@argc: the number of passed arguments
+ *@argv: the string containg all arguments
+ *Return: 0 is success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+	return (0);
+}
