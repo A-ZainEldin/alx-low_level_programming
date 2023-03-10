d7a7da96438ee3960d312085e9101506e135c454
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-10 15:03:35 -0800

    oK

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ *main- a function that prints its arguments
+ *@argc: the number of passed arguments
+ *@argv: the string containg all arguments
+ *Return: 0 is success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+		printf("ERROR");
+	return (0);
+}
