dfc19fdaf17356157b3c8d51204834549ad35da5
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-16 15:44:13 -0700

    ok

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * malloc_checked - cause normal process termination with a status value of 98
+ *malloc_checked - cause normal process termination with a status value of 98
  *
- * @b: allocated memory
+ *@b: allocated memory
  *
- * Return: a pointer to the allocated memory
+ *Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
