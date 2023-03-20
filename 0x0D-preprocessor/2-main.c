f41d8cfd61b5a9eead9057249b20299eaaefde3d
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-20 14:22:08 -0700

    K

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a function that prints its file
- * Return : always 0
+ *main- a function that prints its file
+ *Return: always 0
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
