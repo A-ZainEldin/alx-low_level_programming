92176258d16e4f2dbe3a8f173e46f7b392eff23a
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-22 15:59:38 -0700

    ok

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -5,4 +5,5 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #endif
