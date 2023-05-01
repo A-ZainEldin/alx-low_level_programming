935c0035c63101ea775c1d274a7b3ef37e5cd664
Ahmed Zain Eldeen <ahmedzaineldin5@gmail.com>
2023-05-01 02:41:53 -0700

    ok

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,37 @@
 #include "main.h"
 /**
+ * _strlen - finds the length of a string
+ * @s: pointer to string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen(s + 1) + 1);
+}
+
+/**
+ * _is_palindrome - helper function to check if a string is a palindrome
+ * @s: pointer to string
+ * @i: start index
+ * @j: end index
+ *
+ * Return: 1 if the string is a palindrome, 0 if not
+ */
+int _is_palindrome(char *s, int i, int j)
+{
+	if (i >= j)
+		return (1);
+	else if (s[i] != s[j])
+		return (0);
+	else
+		return (_is_palindrome(s, i + 1, j - 1));
+}
+
+/**
  * is_palindrome - checks if a string is a palindrome
  * @s: pointer to string
  *
@@ -7,21 +39,7 @@
  */
 int is_palindrome(char *s)
 {
-	int len = 0;
+	int len = _strlen(s);
 
-	/* base case: an empty string is a palindrome */
-	if (*s == '\0')
-		return (1);
-
-	/* find the length of the string */
-	while (s[len] != '\0')
-		len++;
-
-	/* check if the first and last characters match */
-	if (s[0] != s[len - 1])
-		return (0);
-
-	/* call the function recursively on the substring between the first and last characters */
-	s[len - 1] = '\0';
-	return (is_palindrome(s + 1));
+	return (_is_palindrome(s, 0, len - 1));
 }
