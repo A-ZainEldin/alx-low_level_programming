c9450db55bc146afa2ae08e032441fbdd41da312
Ahmed Zain Eldeen <ahmedzaineldin5@gmail.com>
2023-05-01 02:29:11 -0700

    new project

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *is_palindrome- works if the function had a palindrome
+ *@s: a pointer to the string
+ *Return: zero if false and one if true
+ */
+int is_palindrome(char *s)
+{
+	int c = 0;
+	int x = 0;
+
+	for (int i = 0; s[i] != '\0'; i++)
+		c++;
+	c -= 1;
+	while (x != c)
+	{
+		if (s[x] != s[c])
+			return (0);
+		x++;
+		c--;
+	}
+	return (1);
+}
