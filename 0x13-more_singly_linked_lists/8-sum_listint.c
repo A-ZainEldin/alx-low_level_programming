99784fc4233f8b4181c6cdce17bea85ca40b549e
Ahmed Zain Eldeen <ahmedzaineldin5@gmail.com>
2023-05-02 14:41:16 -0700

    all

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - calculates the sum 
+ * @head: first node 
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int total = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		total += temp->n;
+		temp = temp->next;
+	}
+
+	return (total);
+}
