68eecc1726854380fb14facea745a7d6ba2f8557
Ahmed Zain Eldeen <ahmedzaineldin5@gmail.com>
2023-05-02 14:07:49 -0700

    ok

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
- * free_listint - sets a list free
+ * free_listint2 - sets a list free
  * @head: the start of list
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
 	if (head == NULL)
 	return;
-	while (head)
+	while (*head)
 	{
-		current = head->next;
-		free(head);
+		current = *head->next;
+		free(*head);
 		head = current;
 	}
+	*head = NULL;
 }
