38ee57b33c216bb67c123d4f6d294edc3f207f57
Ahmed Zain Eldeen <ahmedzaineldin5@gmail.com>
2023-05-04 14:54:55 -0700

    o

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits- count the diffrent number of bits 
+ * @n: first number
+ * @m: second number
+ * Return: number of diffrent bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, num = 0;
+	unsigned long int c;
+	unsigned long int x = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		c = x >> i;
+		if (c & 1)
+			num++;
+	}
+
+	return (num);
+}
