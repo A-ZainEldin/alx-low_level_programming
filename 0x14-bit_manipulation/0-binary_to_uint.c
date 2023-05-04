50c3683beea7bce747475f05f070b8d265b0564e
Ahmed Zain Eldeen <ahmedzaineldin5@gmail.com>
2023-05-04 13:25:40 -0700

    Ok

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int flag = 0, i;
-	unsigned int sum = 0, factor = 1;
+	unsigned int sum = 0;
 
 	for (i = 0; b[i] != '\0'; i++)
 		if (b[i] != '0' && b[i] != '1')
 			flag = 1;
-	if (b == NULL || flag)
+	if (!b || flag)
 		return (0);
 
-	for (i = i - 1; i >= 0; i--)
+	for (i =  0; b[i]; i++)
 	{
-		sum += factor * (b[i]-'0');
-		factor *= 2;
+		sum = 2 * sum + (b[i] - '0');
 	}
 	return (sum);
 }
