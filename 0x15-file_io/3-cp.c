6dd38d7fcc7e8e181a6b09a3efa9ecf9a5363cc8
Ahmed Zain Eldeen <ahmedzaineldin5@gmail.com>
2023-05-08 12:39:12 -0700

    ok

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,95 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void close_file(int fd);
+
+/**
+ * cb - Allocates space.
+ * @file: The name.
+ * Return: the allocated place
+ */
+char *cb(char *name)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", name);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * closeF - Closes file .
+ * @fd: the file to be closed
+ */
+void closeF(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = cb(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	closeF(from);
+	closeF(to);
+
+	return (0);
+}
